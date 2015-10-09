f151c8c1918c3238d29f89afbb618853fa8d23a2
Oliver Halliday <mm10ojh@see4-101.leeds.ac.uk>
2015-10-09 16:34:25 +0100

    updated some experiments, re-done converegence data

---
+++ t-dependent Heating/max_diff_copy.m
@@ -0,0 +1,39 @@
+%function [ w1_row, w2_row, delta_w, delta_diff] = max_diff()
+function [ Height , delta_diff ] = max_diff()
+
+HV_bar       = 20.005;
+T            = 500000000;
+sigma        = 0.05;
+N            = 0.01;
+t            = 1800;
+Height       = [1:1:100];
+
+delta_diff   = [1:1:100];
+% HL_bar  = 21.992;
+% [ xx, zz, psi, ww ] = series_half_sinusoid_plots_4(HL_bar , HV_bar, t, T, sigma);
+% w1_row              = ww(30,:);
+for i = 1:100
+    HL_bar  = 19.993 +  i;
+    [ xx, zz, psi, ww ] = series_half_sinusoid_plots_3(HL_bar , HV_bar, t, T, sigma);
+    w1_row              = ww(30,:);
+
+    HL_bar  = HL_bar + 1;
+    [ xx, zz, psi, ww ] = series_half_sinusoid_plots_3(HL_bar , HV_bar, t, T, sigma);
+    w2_row               = ww(30,:);
+
+    delta_w          = w1_row - w2_row;
+    delta_w          = delta_w .* delta_w;
+    delta_diff(i)    = sqrt(max(delta_w));
+
+end
+figure(2)
+scatter(Height, delta_diff, 'x');
+grid on
+hold on
+title('Maximum difference in vertical velocity vs Lid height');
+xlabel('Lid Height (relative to top of heating)');
+ylabel('Maximum difference in Vertical Velocity');
+
+
+
+return
